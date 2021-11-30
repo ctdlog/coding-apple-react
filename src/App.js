b19f21353d70c654ac4f518291c732dfb0bb129d
CTD <qpflapffhs76@gmail.com>
2021-11-30 23:29:28 +0900

    블로그 제작 & React 기초 문법

--- src/App.js
+++ src/App.js
@@ -84,25 +84,25 @@ function App() {
   );
 }
 
-class Profile extends React.Component {
-  constructor() {
-    super();
-    this.state = { name: "Kim", age: 30 };
-  }
+// class Profile extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = { name: "Kim", age: 30 };
+//   }
 
-  changeName = () => {
-    this.setState({ name: "Park" });
-  };
+//   changeName = () => {
+//     this.setState({ name: "Park" });
+//   };
 
-  render() {
-    return (
-      <div>
-        <h3> 저는 {this.state.name} 입니다 </h3>
-        <button onClick={this.changeName}> 버튼 </button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h3> 저는 {this.state.name} 입니다 </h3>
+//         <button onClick={this.changeName}> 버튼 </button>
+//       </div>
+//     );
+//   }
+// }
 
 function Modal(props) {
   return (
